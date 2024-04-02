1ef239f3e5191958694d46159e7ea9409def9e27
AkiraTorres <torres.thg@gmail.com>
2024-04-02 10:18:33 -0300

    add: client model and migrations

---
+++ migrations/20240401232358-create-clients-table.cjs
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('clients', {
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('clients');
+  }
+};
---
+++ src/models/Client.js
@@ -0,0 +1,24 @@
+import { DataTypes, sequelize } from "../db/Conn.js";
+
+const Client = sequelize.define('clients', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+  },
+);
+
+export default Client;
