899ade005e64d651baf96e728192ba7323989cf2
AkiraTorres <torres.thg@gmail.com>
2024-06-11 19:16:33 -0300

    fix: changed verifyAdmin error message

--- src/middlewares/verifyAdmin.js
+++ src/middlewares/verifyAdmin.js
@@ -1,12 +1,15 @@
 import Admin from '../db/models/Admin.js';
-import EmailDontMatch from '../errors/emailDontMatch.js';
 
 export default async function verifyAdmin(req, res, next) {
   try {
     const { email } = req;
 
     const admin = await Admin.findByPk(email);
-    if (!admin) throw new EmailDontMatch();
+    if (!admin) {
+      const error = new Error('O e-mail informado não pertence a um administrador');
+      error.status = 401;
+      throw error;
+    }
 
     next();
     return true;
