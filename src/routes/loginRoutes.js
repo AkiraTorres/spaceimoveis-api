ef3ca163b973c82a5bf31a20a2577160aeb8989e
AkiraTorres <torres.thg@gmail.com>
2024-06-11 19:26:21 -0300

    add: admin login route

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,9 +1,10 @@
-import Express from 'express';
-import dotenv from 'dotenv';
 import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+import Express from 'express';
 
-import verifyJwt, { blacklist, generateJwt } from '../middlewares/verifyJwt.js';
 import verify from '../middlewares/verifyGoogle.cjs';
+import verifyJwt, { blacklist, generateJwt } from '../middlewares/verifyJwt.js';
+import { findByPk } from '../services/adminService.js';
 import { find } from '../services/globalService.js';
 
 dotenv.config();
@@ -51,6 +52,29 @@ router.post('/google', async (req, res) => {
     const status = error.status || error.code || 500;
     const message = error.message || 'Erro ao se conectar com o banco de dados';
     res.status(status).json(message).end();
+  }
+});
+
+router.post('/login/admin', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const error = new Error('Email ou senha incorretos');
+    error.status = 404;
+
+    if (email === '' || password === '') throw error;
+
+    const user = await findByPk(email, true);
+    if (!user) throw error;
+
+    const isValid = bcrypt.compareSync(password, user.password);
+    if (!isValid) throw error;
+
+    const token = generateJwt(email);
+    const loggedUser = { user, token };
+    res.json(loggedUser);
+  } catch (error) {
+    const status = error.status || 500;
+    res.status(status).json(error.message).end();
   }
 });
 
