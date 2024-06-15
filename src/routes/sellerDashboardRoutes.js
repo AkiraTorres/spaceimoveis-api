a80ba2e5225357f5bee5f3b89d18b4859566689e
AkiraTorres <torres.thg@gmail.com>
2024-06-15 00:04:12 -0300

    fix: add missing export default

--- src/routes/sellerDashboardRoutes.js
+++ src/routes/sellerDashboardRoutes.js
@@ -9,3 +9,5 @@ const router = Express.Router();
 
 router.get('/likes', verifyGoogleToken, verifyJwt, controller.totalPropertiesLikes);
 router.get('/views', verifyGoogleToken, verifyJwt, controller.totalPropertiesViews);
+
+export default router;
