bd78ea7b17aa1774987b1812f9ce065ead93c877
AkiraTorres <torres.thg@gmail.com>
2024-06-24 10:14:29 -0300

    fix: io cors²

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
-    // credentials: true,
+    credentials: true,
     // methods: ["GET", "POST"]
   }
 });
