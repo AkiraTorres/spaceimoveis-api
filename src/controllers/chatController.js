f0784ef9bee7cc5c62141f3804559297584487aa
AkiraTorres <torres.thg@gmail.com>
2024-06-26 10:34:29 -0300

    fix: added error handling to messageController.js and chatController.js

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -3,24 +3,42 @@ import asyncHandler from "express-async-handler";
 import * as service from "../services/chatService.js";
 
 export const createChat = asyncHandler(async (req, res) => {
-  const { email } = req;
-  const { targetEmail } = req.params;
+  try {
+    const { email } = req;
+    const { targetEmail } = req.params;
 
-  const chat = await service.create(email, targetEmail);
-  res.status(201).json(chat);
+    const chat = await service.create(email, targetEmail);
+    res.status(201).json(chat);
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.status !== 500 ? error.message || 'Internal Server Error' : 'Internal Server Error';
+    res.status(status).json({ message });
+  }
 });
 
 export const findUserChats = asyncHandler(async (req, res) => {
-  const { email } = req;
+  try {
+    const { email } = req;
 
-  const chats = await service.findUserChats(email);
-  res.status(200).json(chats);
+    const chats = await service.findUserChats(email);
+    res.status(200).json(chats);
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.status !== 500 ? error.message || 'Internal Server Error' : 'Internal Server Error';
+    res.status(status).json({ message });
+  }
 });
 
 export const findChat = asyncHandler(async (req, res) => {
-  const { email } = req;
-  const { targetEmail } = req.params;
+  try {
+    const { email } = req;
+    const { targetEmail } = req.params;
 
-  const chat = await service.findChat(email, targetEmail);
-  res.status(200).json(chat);
+    const chat = await service.findChat(email, targetEmail);
+    res.status(200).json(chat);
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.status !== 500 ? error.message || 'Internal Server Error' : 'Internal Server Error';
+    res.status(status).json({ message });
+  }
 });
--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -2,27 +2,45 @@ import * as service from "../services/messageService.js";
 import asyncHandler from "express-async-handler";
 
 export const createMessage = asyncHandler(async (req, res) => {
-  const { email } = req;
-  const { chatId } = req.params;
-  const { text } = req.body;
+  try {
+    const { email } = req;
+    const { chatId } = req.params;
+    const { text } = req.body;
 
-  const message = await service.createMessage({chatId, email, text});
-  res.status(201).json(message);
+    const message = await service.createMessage({ chatId, email, text });
+    res.status(201).json(message);
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.status !== 500 ? error.message || 'Internal Server Error' : 'Internal Server Error';
+    res.status(status).json({ message });
+  }
 });
 
 export const findMessages = asyncHandler(async (req, res) => {
-  const { email } = req;
-  const { chatId } = req.params;
+  try {
+    const { email } = req;
+    const { chatId } = req.params;
 
-  const messages = await service.findMessages(chatId, email);
-  res.status(200).json(messages);
+    const messages = await service.findMessages(chatId, email);
+    res.status(200).json(messages);
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.status !== 500 ? error.message || 'Internal Server Error' : 'Internal Server Error';
+    res.status(status).json({ message });
+  }
 });
 
 export const deleteMessage = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const { email } = req;
+  try {
+    const { id } = req.params;
+    const { email } = req;
 
-  await service.deleteMessage(id, email);
-  res.status(204).send();
+    await service.deleteMessage(id, email);
+    res.status(204).send();
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.status !== 500 ? error.message || 'Internal Server Error' : 'Internal Server Error';
+    res.status(status).json({ message });
+  }
 });
 
