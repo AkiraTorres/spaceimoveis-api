5d5eecb6043c9125b7459a5e47cab3198e480674
Thiago Torres <tiago@spaceimoveis.com.br>
2024-09-17 01:47:56 -0300

    fix: create functions to exclude columns from db queries

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -2,4 +2,14 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// Exclude keys from an object
+export function excludeFromObject(obj, keys) {
+  return Object.fromEntries(Object.entries(obj).filter(([key]) => !keys.includes(key)));
+}
+
+// Exclude keys from objects in a list
+export function excludeFromList(objects, keysToDelete) {
+  return objects.map((obj) => excludeFromObject(obj, keysToDelete));
+}
+
 export default prisma;
