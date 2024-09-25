9f97dbe248ccbc301388536839894673ff0cbad1
Thiago Torres <tiago@spaceimoveis.com.br>
2024-09-25 02:21:47 -0300

    fix: middleware to verify if user is admin

--- src/middlewares/verifyAdmin.js
+++ src/middlewares/verifyAdmin.js
@@ -4,14 +4,14 @@ export default async function verifyAdmin(req, res, next) {
   try {
     const { email } = req;
 
-    const admin = await AdminService.find(email, 'admin');
+    const admin = await AdminService.find({ email }, 'admin');
     if (!admin) throw new Error('Unauthorized');
 
     next();
     return true;
   } catch (error) {
     const status = error.status || 401;
-    const message = error.message || 'Unauthorized';
+    const message = 'Usuário deve ser um administrador';
     return res.status(status).json(message).end();
   }
 }
