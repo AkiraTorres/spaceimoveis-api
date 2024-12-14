1eb2eb1ffdf0fdbb1acf8b8555c5718e0d6bbe29
AkiraTorres <torres.thg@gmail.com>
2024-12-14 14:04:14 -0300

    fix: Correct property update syntax in propertyService.js addTimesSeen function

--- src/services/propertyService.js
+++ src/services/propertyService.js
@@ -207,7 +207,8 @@ export default class PropertyService {
     const property = await prisma.property.findFirst({ where: { id: validatedId } });
     if (!property) throw new ConfigurableError('Imóvel não encontrado', 404);
 
-    return prisma.property.update({ where: { id: validatedId } }, { timesSeen: property.timesSeen + 1 });
+    return prisma.property.update({ where: { id: validatedId }, data: { timesSeen: (property.timesSeen + 1) } });
+    // await prisma.sharedProperties.update({ where: { id: sharedProperty.id, email: validatedEmail }, data: { accepted: true } });
   }
 
   static async getMostSeenPropertiesBySeller(email, take = 6) {
